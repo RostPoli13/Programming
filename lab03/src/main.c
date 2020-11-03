16344a62ddc4ec8045ac48dfe8a1f94f6a4ebb10
RostPoli13 <rost.marks13@gmail.com>
2020-11-03 15:01:00 +0200

    testing

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -1,41 +1,41 @@
 int main(){
+
 // Задача №1
 
-#define R1 15 // Сопротивление резистора 1
-#define R2 20 // Сопротивление резистора 2
-#define R3 25 // Сопротивление резистора 3
+#define R1 15 	 // Первое сопротивление
+#define R2 20	 // Второе сопротивление
+#define R3 25	 // Третье сопротивление
 
-float reverse_resistance; // Обратное сопротивление
-float resistance;  	  // Общее сопротивление
+float reverse_r; // Обратное сопротивление
+float resis;  	 // Общее сопротивление
 
-reverse_resistance = ( 1 / R1 ) + ( 1 / R2 ) + ( 1 / R3 );
-resistance = 1 / reverse_resistance '
+reverse_r = ( 1 / R1 ) + ( 1 / R2 ) + ( 1 / R3 );
+resis = 1 / reverse_r;
 
 // Задача №2
 
-#define NUMBER 313 // Трехзначное число
+#define NUMBER 313  // Число
 
-int un;  // Единицы
-int doz; // Десятки
-int hun; // Сотни
-int rev; // Чило в обратном порядке
+int first_num;  // Единицы
+int doz;	    // Десятки
+int hun; 	    // Сотни
+int reverse_number; // Число в обратном порядке
 
-un = NUMBER % 10;
+first_num = ( NUMBER % 10 );
 doz = ( NUMBER % 100 ) / 10;
 hun = NUMBER / 100;
-rev = un * 100 + doz * 10 + hun;
+reverse_number = first_num * 100 + doz * 10 + hun;
 
 // Задача №3
 
-#define A1 3 // Число
+#define N1 3 // Данное число
+int n2;      // Вторая степень
+int n4;      // Четвертая степень
+int n6;      // Шестая степень
+int n8;      // Восьмая степень
+int n10;     // Десятая степень
 
-int n2;  // Число в второй степени
-int n4;  // Число в четвёртой степени
-int n6;  // Число в шестой степени
-int n8;  // Число в восьмой степени
-int n10; // Число в десятой степени
-
-n2 = ( A1 * A1 );
+n2 = ( N1 * N1 );
 n4 = n2 * n2;
 n6 = n4 * n2;
 n8 = n4 * n4;
@@ -43,24 +43,28 @@ n10 = n8 * n2;
 
 // Задача №4
 
+#define NUM 3101 // Данное число
 #define SYS 8 	 // Система счисления
-#define NUM 3101 // Четырёхзначное число
 
-int a = ( NUM / 1000 );
-int b = ( NUM % 1000 ) / 100;
-int c = ( NUM % 100 ) /10;
-int d = ( NUM % 10 );
+int num1; 	 // Единицы
+int num2;	 // Десятки
+int num3;	 // Сотни
+int num4;	 // Тысячи
 
-int ch = ( SYS * SYS * SYS ) * t + ( SYS * SYS ) * b + ( SYS * c ) + d;
+num1 = ( NUM % 10 );
+num2 = ( NUM % 100 ) / 10;
+num3 = ( NUM % 1000 ) / 100;
+num4 = NUM / 1000;
+int sys10 = num1 + ( num2 * SYS ) + ( num3 * SYS * SYS ) + ( num4 * SYS * SYS * SYS );
 
 // Задача №5
 
-#define NUM 789 // Число
+#define NUM 379 // Числа
 
 int first_n = ( NUM / 100 );
 int third_n = ( NUM % 100 ) % 10;
 
-float diff = first_n / ( float ) third_n;
+float diff = first_n / ( float )third_n;
 
 diff *= 100;
 diff = ( int ) diff;
@@ -72,8 +76,6 @@ diff /= 100;
 #define NUM1 30
 #define NUM2 34
 
-int comp = (( NUM1 + NUM2 ) / 2) + ( NUM2 + NUM1 );
-
-
+int dob =  ( NUM1 * NUM2 ) * (( NUM1 + NUM2 ) / 2 ) * ( NUM2 + NUM1 +1 );
 return 0;
 }
