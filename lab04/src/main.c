d3f8e32a704126b81628dd487b930c8baeaf5f13
RostPoli13 <rost.marks13@gmail.com>
2020-11-16 23:01:55 +0200

    StartWork

---
+++ lab04/src/main.c
@@ -0,0 +1,19 @@
+
+int main (){
+  	//Task 1
+	#define R1 5
+	#define PI 3.14
+	int c;
+	int s;
+	int v;
+	if () {
+	c = ( 2 * PI ) * R1;
+	}
+	else if () {
+	s = PI * ( R1 * R1 );
+	}
+	else (){
+	v= 4/3 * PI * ( R1 * R1 * R1 );
+}
+return 0;
+}
