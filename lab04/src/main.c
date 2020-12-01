1f015fb5e42f5972d45bc93f215f699799fb32ea
sicho <sicho@Shi.Ki>
2020-12-01 19:06:36 +0200

    Edit1

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,19 +1,21 @@
-
+#define R1 5
+#define PI 3.14
 int main (){
   	//Task 1
-	#define R1 5
-	#define PI 3.14
 	int c;
-	int s;
-	int v;
-	if () {
-	c = ( 2 * PI ) * R1;
+	float s;
+	float v;
+	int l = 1 ;
+	int S = 1 ;
+	int V = 1 ;
+	if ( l = 1) {
+	c = ( 2. * PI ) * R1;
 	}
-	else if () {
+	if ( S = 1 ) {
 	s = PI * ( R1 * R1 );
 	}
-	else (){
-	v= 4/3 * PI * ( R1 * R1 * R1 );
+	if ( V = 1 ){
+	v= 4. / 3. * PI * ( R1 * R1 * R1 );
 }
 return 0;
 }
