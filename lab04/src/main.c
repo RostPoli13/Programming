6ad3907d99b607580bf9b7768e606fc850283f88
RostPoli13 <rost.marks13@gmail.com>
2020-12-24 18:02:47 +0200

    Final

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -18,7 +18,7 @@ int main (){
 	}
 	if ( V = 1 ){
 	v= 4. / 3. * PI * ( R1 * R1 * R1 );
-
+	}
 	// Task 2
 
 	int x = 113;
@@ -29,6 +29,45 @@ int main (){
         d = 423. / 113. ;
         cut = d * 100;
         t = cut / 100. ;
+
+	// Task 3
+
+	int k = 5;
+	int m = 10;
+	int n = 15;
+	if (k >= m){
+		m += 1;
+}
+	if (m >= n){
+		n += 1 ;
+}
+	// Task 4
+
+	float x = 3 ;
+	
+	if (x < -1) {
+		y =  -1/x;
+}
+	else if (x > -1 && x < 1){
+		y = x * x;
+}
+ 	else ( x = -1 || x >= 1 ){
+		y = 1 
+}
+	// Task 5
+
+	int y[9];
+	for (int x = -3 , a = 0 ; x <= ; x++ , a++)
+{
+	if (x == 0){
+	y[a] = 1;
+	}
+	else if (x >= 1 ){
+	y[a] = x - 1;
+	}
+	else if	(x <= -1 ){
+	y[a] = x + 1;
+	}
 }
 return 0;
 }
